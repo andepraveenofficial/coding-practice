0ba5d91657635cd39cf63eee2392c3b49d893602
Ande Praveen <praveenande2023@gmail.com>
2024-06-01 12:56:15 +0530

    01 Most Frequent Character in a String

---
+++ 01-Most-Frequent-Character-in-a-String.js
@@ -0,0 +1,43 @@
+// 01 Most Frequent Character in a String
+
+/*
+description :
+
+Given a string, find the character that appears the most frequently. 
+If multiple characters have the same frequency, return any one of them.
+
+*/
+
+
+const str = "abracadabra";
+
+const obj = {};
+
+let maximum = str[0];
+
+for (let char of str){
+    if (!obj[char]){
+        obj[char] = 1;
+    }
+    else{
+        obj[char]++;
+    }
+
+    if (obj[char] > obj[maximum]){
+        maximum = char;
+    }
+}
+
+
+console.log(maximum);
+
+
+
+
+
+
+
+
+
+
+
