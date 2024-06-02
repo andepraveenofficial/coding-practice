a9fed0d669b3b849686066e65896dbb6a0076934
Ande Praveen <praveenande2023@gmail.com>
2024-06-02 14:29:17 +0530

    14 Question Pending

---
+++ 14-Find-Heavy-Ball-Among-8.js
@@ -0,0 +1,11 @@
+// 14 Find Heavy Ball Among 8
+
+/*
+Description :
+
+Given 8 balls, where 7 have equal weight and 1 is heavier, determine the minimum number of iterations to find the heavy ball.	
+*/
+
+let balls = [1, 1, 1, 2, 1, 1, 1, 1];
+
+// Pending
