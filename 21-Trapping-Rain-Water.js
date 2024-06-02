eb78e4281feab2a7bdf8e6517020776f9bdc925f
Ande Praveen <praveenande2023@gmail.com>
2024-06-02 16:15:34 +0530

    21 Trapping Rain Water

---
+++ 21-Trapping-Rain-Water.js
@@ -0,0 +1,28 @@
+// 21 Trapping Rain Water
+
+/*
+Description :
+
+Given n non-negative integers representing an elevation map, find how much water it can trap after raining.
+*/
+
+const arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+
+let result = 0
+for (let i=0; i<arr.length; i++){
+    const mainPart = arr.slice(0, i+1);
+    console.log("-----------")
+    console.log(mainPart)
+    for (let j=0; j<mainPart.length; j++){
+        const subPart = mainPart.slice(-j-1, mainPart.length);
+        console.log(subPart)
+        if (!subPart.includes(0)){
+            if (result < subPart.length){
+                result = subPart.length
+            }
+        }
+    }
+    console.log("-----------")
+}
+
+console.log(result);  // 6
