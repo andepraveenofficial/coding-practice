7a787ec4c7b34d786fa327395d63c1fc5af4b728
Ande Praveen <praveenande2023@gmail.com>
2024-06-02 11:40:00 +0530

    08 Sort Map by Values

--- 08-Longest-Common-Prefix.js
+++ 08-Longest-Common-Prefix.js
@@ -1,9 +1,14 @@
 // 08 Longest Common Prefix	
+// 08 Sort Map by Values
 
 /*
 Description :
  
+08 Longest Common Prefix :
 Given an array of strings, find the longest common prefix. If there is no common prefix, return an empty string.	
+
+08 Sort Map by Values :
+Given a map, sort it by its values and return the sorted map.
 */
 
 const arr = ["flower", "flow", "flight"];
@@ -27,3 +32,31 @@ while (flag){
 }
 
 console.log(prefix); // fl
+
+/* -----------------------------------------*/
+
+let originalMap = { "a": 3, "b": 1, "c": 2 };
+
+
+
+let objectEntries = Object.entries(originalMap);
+console.log(objectEntries);
+
+for (let i=0; i<objectEntries.length; i++){
+    let flag = true;
+    for (let j=0; j<objectEntries.length-1; j++){
+        if (objectEntries[j][1] > objectEntries[j+1][1]){
+            [objectEntries[j], objectEntries[j+1]] = [objectEntries[j+1], objectEntries[j]]
+            flag = false;
+        }
+    }
+
+    if (!flag){
+        break;
+    }
+}
+
+console.log(objectEntries);
+
+let sortedObject = Object.fromEntries(objectEntries);
+console.log(sortedObject);  // { b: 1, c: 2, a: 3 }
